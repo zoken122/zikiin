76df7fdde94399d82a302346c53b93e893d95d9e
zoken122 <zok0707gg@gmail.com>
2024-06-08 00:05:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const moreInfoBtn = document.getElementById("more-info-btn");
+
+    moreInfoBtn.addEventListener("click", function() {
+        alert("Спасибо за интерес! Мы скоро добавим больше информации.");
+    });
+});
